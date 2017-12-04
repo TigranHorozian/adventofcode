a4f00263e44c6ebb54d688250558812c7253417c
Tigran HOROZIAN <thorozian@softbankrobotics.com>
2017-12-04 13:48:48 +0100

    Create day3.py file

---
+++ src_python/day3.py
@@ -0,0 +1,14 @@
+# -*- encoding: UTF-8 -*-
+"""
+Day3 source code
+
+:Author: Tigran Horozian
+:email: tigran.horozian@gmail.com
+"""
+
+
+
+if __name__ == '__main__':
+    input =
+
+    print("Sum of max min differences is : " )
